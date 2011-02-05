6a0009a03e627c1bc611e66701f00d40e0b5bd48
John Nunemaker <nunemaker@gmail.com>
2011-02-05 18:14:34 -0500

    Minor: whitespace.

--- examples/mongo.rb
+++ examples/mongo.rb
@@ -7,6 +7,7 @@ root_path = Pathname(__FILE__).dirname.join('..').expand_path
 lib_path  = root_path.join('lib')
 $:.unshift(lib_path)
 require 'toystore'
+
 class User
   include Toy::Store
   store :mongo, Mongo::Connection.new.db('adapter')['testing']
