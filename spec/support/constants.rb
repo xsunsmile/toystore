d745fcdd99b960172cd13819616ec3e445017f35
John Nunemaker <nunemaker@gmail.com>
2012-04-18 13:28:33 -0400

    Allow passing superclass to model and create_constant for specs.

--- spec/support/constants.rb
+++ spec/support/constants.rb
@@ -16,17 +16,21 @@ module Support
       constants.each { |constant| remove_constant(constant) }
     end
 
-    def create_constant(constant)
+    def create_constant(constant, superclass=nil)
       remove_constant(constant)
-      Kernel.const_set(constant, Model(constant))
+      Kernel.const_set(constant, Model(constant, superclass))
     end
 
     def remove_constant(constant)
       Kernel.send(:remove_const, constant) if Kernel.const_defined?(constant)
     end
 
-    def Model(name=nil)
-      Class.new.tap do |model|
+    def Model(name=nil, superclass=nil)
+      if superclass.nil?
+        Class.new
+      else
+        Class.new(superclass)
+      end.tap do |model|
         model.class_eval """
           def self.name; '#{name}' end
           def self.to_s; '#{name}' end
