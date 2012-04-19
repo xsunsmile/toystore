2fdc18b8d8428a932c1e5eeafa6a4db2269f1473
John Nunemaker <nunemaker@gmail.com>
2012-04-19 10:13:40 -0400

    Added pretty inspect for classes.

--- lib/toy/inspect.rb
+++ lib/toy/inspect.rb
@@ -2,6 +2,17 @@ module Toy
   module Inspect
     extend ActiveSupport::Concern
 
+    module ClassMethods
+      def inspect
+        keys = attributes.keys - ['id']
+        nice_string = keys.sort.map do |name|
+          type = attributes[name].type
+          "#{name}:#{type}"
+        end.join(" ")
+        "#{name}(id:#{attributes['id'].type} #{nice_string})"
+      end
+    end
+
     def inspect
       attributes_as_nice_string = self.class.attributes.keys.map(&:to_s).sort.map do |name|
         "#{name}: #{read_attribute(name).inspect}"
--- spec/toy/inspect_spec.rb
+++ spec/toy/inspect_spec.rb
@@ -8,8 +8,16 @@ describe Toy::Attributes do
     User.attribute(:age, Integer)
   end
 
-  it "prints out object id and attributes sorted with values" do
-    user = User.new(:age => 28, :name => 'John')
-    user.inspect.should == %Q(#<User:#{user.object_id} age: 28, id: "#{user.id}", name: "John">)
+  describe ".inspect" do
+    it "prints out attribute names and types" do
+      User.inspect.should == %Q(User(id:String age:Integer name:String))
+    end
+  end
+
+  describe "#inspect" do
+    it "prints out object id and attributes sorted with values" do
+      user = User.new(:age => 28, :name => 'John')
+      user.inspect.should == %Q(#<User:#{user.object_id} age: 28, id: "#{user.id}", name: "John">)
+    end
   end
 end
